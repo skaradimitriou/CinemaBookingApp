94fe659d23842c509463fd2aa2eff421ec875b17
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-09-17 23:18:52 +0300

    Abstraction & Basic Libraries | Init

---
+++ app/src/main/java/com/stathis/cinemabooking/abstraction/AbstractActivity.kt
@@ -0,0 +1,26 @@
+package com.stathis.cinemabooking.abstraction
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class AbstractActivity(layoutId : Int) : AppCompatActivity(layoutId) {
+
+    abstract fun init()
+    abstract fun startOps()
+    abstract fun stopOps()
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        init()
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        startOps()
+    }
+
+    override fun onStop() {
+        stopOps()
+        super.onStop()
+    }
+}
